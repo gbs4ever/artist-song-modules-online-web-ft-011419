09dd8ddbd5ea837d41f47835fd3ae14e7a84f35f
Sim Greenbaum <gbs4ever2@gmail.com>
2019-01-30 16:35:42 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -13,9 +13,6 @@ include  Paramable
     @songs = []
   end
 
-  #def self.find_by_name(name)
-  #  @@artists.detect{|a| a.name == name}
-  #end
 
   def self.all
     @@artists
@@ -31,7 +28,5 @@ include  Paramable
     songs.each { |song| add_song(song) }
   end
 
-  #def to_param
-  #  name.downcase.gsub(' ', '-')
-  #end
+  
 end
