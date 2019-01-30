bb1c45559d9eb685389b24981414e675ec1e1d7f
Sim Greenbaum <gbs4ever2@gmail.com>
2019-01-30 16:27:18 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,6 +4,7 @@ class Artist
   attr_accessor :name
   attr_reader :songs
 extend  Memorable
+extend Findable
   @@artists = []
 
   def initialize
@@ -29,7 +30,7 @@ extend  Memorable
     songs.each { |song| add_song(song) }
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+  #def to_param
+  #  name.downcase.gsub(' ', '-')
+  #end
 end
